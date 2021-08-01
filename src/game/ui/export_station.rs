c901416c0848476c305b94b6e151a2d272cf9ca3
izhevskoye <ptrhaider7@gmail.com>
2021-08-01 22:52:53 +0200

    #78 adjust export

--- src/game/ui/export_station.rs
+++ src/game/ui/export_station.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext};
+use collecting_hashmap::CollectingHashMap;
 
 use crate::game::{
     assets::ExportStation, current_selection::CurrentlySelected,
@@ -21,26 +22,51 @@ pub fn edit_ui(
             egui::Window::new("ExportStation").show(egui_context.ctx(), |ui| {
                 ui.heading("Export Station");
 
+                let mut groups = CollectingHashMap::new();
                 for (id, resource) in resources.iter() {
-                    if ui
-                        .button(format!(
-                            "{}: {}",
-                            resource.name,
-                            if export.goods.contains(id) {
-                                "Yes"
-                            } else {
-                                "No"
+                    groups.insert(resource.group.clone(), (id, resource));
+                }
+
+                let mut group_names: Vec<String> = groups.keys().cloned().collect();
+                group_names.sort_by_key(|a| a.to_lowercase());
+
+                for group in group_names.iter() {
+                    let resources = groups.remove_all(group).unwrap();
+
+                    let count = resources
+                        .iter()
+                        .filter(|(id, _resource)| export.goods.contains(id))
+                        .count();
+
+                    egui::CollapsingHeader::new(format!("{} ({})", group, count))
+                        .id_source(group)
+                        .show(ui, |ui| {
+                            for (id, resource) in resources.iter() {
+                                if ui
+                                    .button(format!(
+                                        "{}: {}",
+                                        resource.name,
+                                        if export.goods.contains(id) {
+                                            "Yes"
+                                        } else {
+                                            "No"
+                                        }
+                                    ))
+                                    .clicked()
+                                {
+                                    if export.goods.contains(&id) {
+                                        export.goods = export
+                                            .goods
+                                            .iter()
+                                            .cloned()
+                                            .filter(|r| r != *id)
+                                            .collect();
+                                    } else {
+                                        export.goods.push(id.to_string());
+                                    }
+                                }
                             }
-                        ))
-                        .clicked()
-                    {
-                        if export.goods.contains(&id) {
-                            export.goods =
-                                export.goods.iter().cloned().filter(|r| r != id).collect();
-                        } else {
-                            export.goods.push(id.clone());
-                        }
-                    }
+                        });
                 }
             });
         }
